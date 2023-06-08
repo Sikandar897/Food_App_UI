54ea8d1f5a91d36a74fc8bd902b2fbd799730b7a
Sikandar897 <sikandarafridi6@gmail.com>
2023-06-08 12:38:20 -0700

    incomplete catalog modle have some error fix 2mro

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,43 @@
+// ignore_for_file: unused_local_variable, avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:flutter_application_1/widgets/item_widget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    try {
+      final catalogJson =
+          await rootBundle.loadString("assets/file/catalog.json");
+      final decodedData = jsonDecode(catalogJson);
+      var productsData = decodedData["products"];
+      CatalogModel.items = List.from(productsData)
+          .map<Item>((item) => Item.fromMap(item))
+          .toList();
+      setState(() {});
+    } catch (error) {
+      print("Error loading catalog data: $error");
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
@@ -17,10 +46,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: dummyList.length,
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
             return ItemWidget(
-              item: dummyList[index],
+              item: CatalogModel.items[index],
             );
           },
         ),
