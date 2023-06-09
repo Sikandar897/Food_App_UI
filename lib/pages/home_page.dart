3a5c3c8dd970b251bf617c76188033a5ac01b2a1
Sikandar897 <sikandarafridi6@gmail.com>
2023-06-09 12:15:38 -0700

    add to cart done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,21 @@
-// ignore_for_file: unused_local_variable, avoid_print, unnecessary_null_comparison
+// ignore_for_file: unnecessary_null_comparison, avoid_print
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
-import 'package:flutter_application_1/widgets/item_widget.dart';
 import 'dart:convert';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<Item> _cartItems = [];
+
   @override
   void initState() {
     super.initState();
@@ -37,21 +38,136 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  void addToCart(Item item) {
+    setState(() {
+      _cartItems.add(item);
+    });
+  }
+
+  void removeFromCart(Item item) {
+    setState(() {
+      _cartItems.remove(item);
+    });
+  }
+
+  void clearCart() {
+    setState(() {
+      _cartItems.clear();
+    });
+  }
+
+  double getCartTotal() {
+    double total = 0;
+    for (var item in _cartItems) {
+      total += item.price;
+    }
+    return total;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return SizedBox(
+                    height: 300,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: const Text(
+                            'Cart',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.clear),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                        const Divider(),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: _cartItems.length,
+                            itemBuilder: (context, index) {
+                              final cartItem = _cartItems[index];
+                              return ListTile(
+                                title: Text(cartItem.name),
+                                subtitle: Text(
+                                    '\$${cartItem.price.toStringAsFixed(2)}'),
+                                trailing: IconButton(
+                                  icon: const Icon(Icons.remove_shopping_cart),
+                                  onPressed: () {
+                                    removeFromCart(cartItem);
+                                  },
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        const Divider(),
+                        ListTile(
+                          title: const Text(
+                            'Total',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(
+                            '\$${getCartTotal().toStringAsFixed(2)}',
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: () {
+                            clearCart();
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Clear Cart'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
             ? ListView.builder(
-                itemCount: CatalogModel.items.length, // Update this line
+                itemCount: CatalogModel.items.length,
                 itemBuilder: (context, index) {
-                  return ItemWidget(
-                    item: CatalogModel.items[index],
+                  final item = CatalogModel.items[index];
+                  final isInCart = _cartItems.contains(item);
+                  return GestureDetector(
+                    onTap: () {
+                      if (!isInCart) {
+                        addToCart(item);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Item added to cart'),
+                            duration: Duration(seconds: 1),
+                          ),
+                        );
+                      }
+                    },
+                    child: ItemWidget(
+                      item: item,
+                      isInCart: isInCart,
+                    ),
                   );
                 },
               )
@@ -60,6 +176,29 @@ class _HomePageState extends State<HomePage> {
               ),
       ),
       drawer: const MyDrawer(),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+  final bool isInCart;
+
+  const ItemWidget({
+    super.key,
+    required this.item,
+    required this.isInCart,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.network(item.image),
+      title: Text(item.name),
+      subtitle: Text('\$${item.price.toStringAsFixed(2)}'),
+      trailing: isInCart
+          ? const Icon(Icons.check_circle)
+          : const Icon(Icons.add_circle),
     );
   }
 }
